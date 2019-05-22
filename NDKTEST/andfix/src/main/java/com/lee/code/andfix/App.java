24fe2e249642f80655f78daf467d7d84f6b4ac15
jv.lee@foxmail.com <jv.lee@foxmail.com>
2019-05-22 22:48:58 +0800

    add code

---
+++ NDKTEST/andfix/src/main/java/com/lee/code/andfix/App.java
@@ -0,0 +1,16 @@
+package com.lee.code.andfix;
+
+import android.app.Application;
+
+/**
+ * @author jv.lee
+ * @date 2019-05-22
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ NDKTEST/andfix/src/main/java/com/lee/code/andfix/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lee.code.andfix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author jv.lee
+ */
+public class MainActivity
+        extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
