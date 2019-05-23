3b963710d6193e4b19c92693893fbee16b4ebd62
lijiawei@hclw.online <lijiawei@hclw.online>
2019-05-23 18:03:00 +0800

    update code

---
+++ LeeLibrary/library/src/main/java/com/lee/library/widget/banner/transformer/CustomTransformer.java
@@ -0,0 +1,28 @@
+package com.lee.library.widget.banner.transformer;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ *
+ * @author Administrator
+ * @date 2017/8/15
+ */
+
+public class CustomTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.9F;
+    @Override
+    public void transformPage(View page, float position) {
+
+        if(position < -1){
+            page.setScaleY(MIN_SCALE);
+        }else if(position<= 1){
+            float scale = Math.max(MIN_SCALE,1 - Math.abs(position));
+            page.setScaleY(scale);
+
+        }else{
+            page.setScaleY(MIN_SCALE);
+        }
+    }
+
+}
