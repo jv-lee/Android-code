c783253bbeea890c9d48ed4615967f256a5a5d68
lijiawei@hclw.online <lijiawei@hclw.online>
2019-05-24 17:15:35 +0800

    add banner or marquee widget

---
+++ LeeLibrary/app/src/main/java/com/lee/code/BannerAndMarqueeFragment.java
@@ -0,0 +1,92 @@
+package com.lee.code;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.lee.library.base.BaseFragment;
+import com.lee.library.ioc.annotation.ContentView;
+import com.lee.library.ioc.annotation.InjectView;
+import com.lee.library.widget.MarqueeView;
+import com.lee.library.widget.banner.MZBannerView;
+import com.lee.library.widget.banner.holder.ContentBannerViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * @author jv.lee
+ */
+@ContentView(R.layout.fragment_banner_and_marquee)
+public class BannerAndMarqueeFragment extends BaseFragment {
+
+    @InjectView(R.id.banner)
+    MZBannerView<String> banner;
+    @InjectView(R.id.marquee)
+    MarqueeView marquee;
+
+    private List<String> data;
+    private List<String> marqueeData;
+
+    public BannerAndMarqueeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    protected void bindData(Bundle savedInstanceState) {
+        data = new ArrayList<>();
+        data.add("https://cdn.pixabay.com/photo/2019/05/01/09/54/pink-4170404__340.jpg");
+        data.add("https://cdn.pixabay.com/photo/2015/12/08/00/28/elephants-1081749__340.jpg");
+        data.add("https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733__340.jpg");
+        data.add("https://cdn.pixabay.com/photo/2017/01/14/16/36/city-1979892__340.jpg");
+        data.add("https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg");
+
+        marqueeData = new ArrayList<>();
+        marqueeData.add("这是我第一次中奖~");
+        marqueeData.add("我又中奖了！！！");
+        marqueeData.add("3连发？？ 还是我啊~");
+        marqueeData.add("什么鬼 怎么都是我中奖~");
+        marqueeData.add("人气大爆发？？？~");
+        marqueeData.add("我都不敢相信还是我了！！！~");
+    }
+
+    @Override
+    protected void bindView() {
+        banner.setPages(data, ContentBannerViewHolder::new);
+        marquee.bindData(marqueeData);
+    }
+
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    @Override
+    protected void onFragmentResume() {
+        super.onFragmentResume();
+        if (banner != null) {
+            banner.start();
+        }
+        if (marquee != null) {
+            marquee.start();
+        }
+    }
+
+    @Override
+    protected void onFragmentPause() {
+        super.onFragmentPause();
+        if (banner != null) {
+            banner.pause();
+        }
+        if (marquee != null) {
+            marquee.pause();
+        }
+    }
+
+}
---
+++ LeeLibrary/library/src/main/java/com/lee/library/widget/MarqueeView.java
@@ -0,0 +1,138 @@
+package com.lee.library.widget;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.lee.library.R;
+import com.lee.library.utils.SizeUtil;
+
+import java.util.List;
+
+/**
+ * @author jv.lee
+ * @date 2019/5/24.
+ * description：
+ */
+public class MarqueeView extends FrameLayout {
+
+    private List<String> data;
+    private int currentIndex = 0;
+
+    private TextView tvContent;
+    private Drawable leftDrawable;
+    private int fontColor;
+    private float fontSize;
+
+    private int width;
+    private int height;
+    private AnimatorSet set;
+    private boolean hasStop = false;
+
+    public MarqueeView(Context context) {
+        this(context, null);
+    }
+
+    public MarqueeView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MarqueeView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        readAttrs(context, attrs);
+        initView();
+    }
+
+    private void readAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MarqueeView);
+        fontColor = typedArray.getColor(R.styleable.MarqueeView_font_color, Color.WHITE);
+        fontSize = typedArray.getDimension(R.styleable.MarqueeView_font_size, SizeUtil.px2sp(getContext(), 16));
+        leftDrawable = typedArray.getDrawable(R.styleable.MarqueeView_left_drawable);
+        typedArray.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = MeasureSpec.makeMeasureSpec(widthMeasureSpec,
+                MeasureSpec.UNSPECIFIED);
+        height = MeasureSpec.makeMeasureSpec(heightMeasureSpec,
+                MeasureSpec.UNSPECIFIED);
+        initAnim();
+    }
+
+    private void initView() {
+        tvContent = new TextView(getContext());
+        tvContent.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        tvContent.setGravity(Gravity.CENTER_VERTICAL);
+        tvContent.setTextSize(SizeUtil.px2sp(getContext(), fontSize));
+        tvContent.setTextColor(fontColor);
+        if (leftDrawable != null) {
+            leftDrawable.setBounds(0, 0, leftDrawable.getMinimumWidth(), leftDrawable.getMinimumHeight());
+            tvContent.setCompoundDrawables(leftDrawable, null, null, null);
+        }
+        addView(tvContent);
+    }
+
+    private void initAnim() {
+        ObjectAnimator in = ObjectAnimator.ofFloat(tvContent, View.TRANSLATION_Y, height, 0f);
+        ObjectAnimator out = ObjectAnimator.ofFloat(tvContent, View.TRANSLATION_Y, 0f, -height);
+        out.setStartDelay(500);
+        set = new AnimatorSet();
+        set.play(in).before(out);
+        set.setDuration(1000);
+
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                tvContent.setText(data.get(currentIndex++));
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (!hasStop) {
+                    if (currentIndex < data.size()) {
+                        set.start();
+                    } else {
+                        currentIndex = 0;
+                        set.start();
+                    }
+                }
+            }
+        });
+    }
+
+    public void bindData(List<String> data) {
+        if (set != null) {
+            currentIndex = 0;
+            pause();
+        }
+        this.data = data;
+    }
+
+    public void start() {
+        if (data == null || data.size() == 0) {
+            return;
+        }
+        hasStop = false;
+        set.start();
+    }
+
+    public void pause() {
+        hasStop = true;
+        set.end();
+    }
+
+}
+
