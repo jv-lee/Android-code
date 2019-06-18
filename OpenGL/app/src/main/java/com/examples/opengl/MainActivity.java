0a9627d572204f76747094fa2213f485b377c912
lijiawei@hclw.online <lijiawei@hclw.online>
2019-06-18 09:55:23 +0800

    insert opengl

---
+++ OpenGL/app/src/main/java/com/examples/opengl/MainActivity.java
@@ -0,0 +1,16 @@
+package com.examples.opengl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author jv.lee
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
