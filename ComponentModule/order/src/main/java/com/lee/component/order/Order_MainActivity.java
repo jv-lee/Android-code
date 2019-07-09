38330a6434c12453ab35f07d26dd563029d0dd84
jv.lee@foxmail.com <jv.lee@foxmail.com>
2019-07-09 23:07:12 +0800

    add component model

---
+++ ComponentModule/order/src/main/java/com/lee/component/order/Order_MainActivity.java
@@ -0,0 +1,17 @@
+package com.lee.component.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * @author jv.lee
+ */
+public class Order_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity__main);
+    }
+}
