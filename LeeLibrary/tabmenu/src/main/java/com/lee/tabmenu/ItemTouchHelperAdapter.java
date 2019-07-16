a74a0ab725a5cc02c9f3b8bd9b0c069f30737c56
lijiawei@hclw.online <lijiawei@hclw.online>
2019-07-16 19:08:58 +0800

    update code

--- LeeLibrary/tabmenu/src/main/java/com/lee/tabmenu/ItemTouchHelperAdapter.java
+++ LeeLibrary/tabmenu/src/main/java/com/lee/tabmenu/ItemTouchHelperAdapter.java
@@ -1,8 +1,18 @@
 package com.lee.tabmenu;
 
 public interface ItemTouchHelperAdapter {
+
+    /**
+     * item移动
+     * @param formPosition
+     * @param toPosition
+     */
     void onItemMove(int formPosition, int toPosition);
 
+    /**
+     * item删除
+     * @param position
+     */
     void onItemRemove(int position);
 
 }
