dce266e8ebdfbfc15fb163e0fbbf039722fc618f
lijiawei@hclw.online <lijiawei@hclw.online>
2019-09-26 14:07:58 +0800

    add kotlin coroutines

--- KotlinLibrary/library/src/main/java/com/lee/library/mvvm/BaseViewModel.kt
+++ KotlinLibrary/library/src/main/java/com/lee/library/mvvm/BaseViewModel.kt
@@ -2,14 +2,76 @@ package com.lee.library.mvvm
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
+import java.util.concurrent.CancellationException
 
 /**
  * @author jv.lee
  * @date 2019-08-15
  * @description
  */
-abstract class BaseViewModel(application: Application) : AndroidViewModel(application) {
+open class BaseViewModel(application: Application) : AndroidViewModel(application) {
+
+    val mException: MutableLiveData<Throwable> = MutableLiveData()
+
+    private fun launchOnUI(block: suspend CoroutineScope.() -> Unit) {
+        GlobalScope.launch(Dispatchers.Main) {
+            block()
+        }
+    }
+
+    suspend fun <T> launchIO(block: suspend CoroutineScope.() -> T) {
+        withContext(Dispatchers.IO) {
+            block
+        }
+    }
+
+    fun launch(tryBlock: suspend CoroutineScope.() -> Unit) {
+        launchOnUI {
+            tryCatch(tryBlock, {}, {}, true)
+        }
+    }
+
+    fun launchOnUITryCatch(tryBlock: suspend CoroutineScope.() -> Unit,
+                           catchBlock: suspend CoroutineScope.(Throwable) -> Unit,
+                           finallyBlock: suspend CoroutineScope.() -> Unit,
+                           handleCancellationExceptionManually: Boolean
+    ) {
+        launchOnUI {
+            tryCatch(tryBlock, catchBlock, finallyBlock, handleCancellationExceptionManually)
+        }
+    }
+
+    fun launchOnUITryCatch(tryBlock: suspend CoroutineScope.() -> Unit,
+                           handleCancellationExceptionManually: Boolean = false
+    ) {
+        launchOnUI {
+            tryCatch(tryBlock, {}, {}, handleCancellationExceptionManually)
+        }
+    }
+
+    private suspend fun tryCatch(
+        tryBlock: suspend CoroutineScope.() -> Unit,
+        catchBlock: suspend CoroutineScope.(Throwable) -> Unit,
+        finallyBlock: suspend CoroutineScope.() -> Unit,
+        handleCancellationExceptionManually: Boolean = false
+    ) {
+        coroutineScope {
+            try {
+                tryBlock()
+            } catch (e: Throwable) {
+                if (e !is CancellationException || handleCancellationExceptionManually) {
+                    mException.value = e
+                    catchBlock(e)
+                } else {
+                    throw e
+                }
+            } finally {
+                finallyBlock()
+            }
+        }
+    }
 
 }
 
