47de58cf335dbe2efa5f0b88ab3a171f3288026a
jv.lee@foxmail.com <jv.lee@foxmail.com>
2019-12-21 16:22:30 +0800

    update den utils

--- LeeLibrary/library/src/main/java/com/lee/library/utils/DensityUtil.java
+++ LeeLibrary/library/src/main/java/com/lee/library/utils/DensityUtil.java
@@ -4,15 +4,17 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.ComponentCallbacks;
 import android.content.res.Configuration;
-import android.os.Bundle;
 import android.util.DisplayMetrics;
 
+/**
+ * @author jv.lee
+ */
 public class DensityUtil {
 
     /**
      * 参考设备的宽，单位：dp
      */
-    private static final float WIDTH = 360;
+    private static final float WIDTH = 375;
 
     /**
      * 表示屏幕密度
@@ -23,6 +25,11 @@ public class DensityUtil {
      */
     private static float appScaleDensity;
 
+    /**
+     * 修改当前activity的缩放比例 调整dpi值
+     * @param application
+     * @param activity
+     */
     public static void setDensity(final Application application, Activity activity) {
         //获取当前app的屏幕显示信息
         DisplayMetrics displayMetrics = application.getResources().getDisplayMetrics();
@@ -59,6 +66,17 @@ public class DensityUtil {
         dm.scaledDensity = targetScaleDensity;
         dm.densityDpi = targetDensityDpi;
 
+    }
+
+    /**
+     * 保证在应用进入热启动之前 清除所以density修改 保证热启动闪屏ui 不被 density和系统初始化的density发生拉扯情况
+     * @param activity
+     */
+    public static void resetDensity(Activity activity) {
+        DisplayMetrics dm = activity.getResources().getDisplayMetrics();
+        dm.density = appDensity;
+        dm.scaledDensity = appScaleDensity;
+        dm.densityDpi = (int)(appDensity * 160);
     }
 
 }
