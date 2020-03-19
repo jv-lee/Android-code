61dd0e383f1e3c83d5599846bcb9d166a351e935
lijiawei <lijiawei@52zhifan.net>
2020-03-19 14:41:39 +0800

    insert coroutine module

---
+++ Coroutine/app/src/main/java/com/lee/coroutine/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lee.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * @author jv.lee
+ * @date 2020.03.19
+ * @description
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
