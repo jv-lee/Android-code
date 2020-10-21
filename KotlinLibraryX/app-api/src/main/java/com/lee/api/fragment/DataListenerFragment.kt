ca6c745d502c8fbce27481b43559d917417f9b50
jv.lee <jv.lee@foxmail.com>
2020-10-21 10:16:27 +0800

    添加Fragment sendData示例.

--- KotlinLibraryX/app-api/src/main/java/com/lee/api/fragment/DataListenerFragment.kt
+++ KotlinLibraryX/app-api/src/main/java/com/lee/api/fragment/DataListenerFragment.kt
@@ -1,5 +1,6 @@
 package com.lee.api.fragment
 
+import androidx.fragment.app.FragmentResultListener
 import com.lee.api.R
 import com.lee.api.databinding.FragmentDataListenerBinding
 import com.lee.library.base.BaseFragment
@@ -9,7 +10,12 @@ import com.lee.library.mvvm.base.BaseViewModel
 class DataListenerFragment :
     BaseFragment<FragmentDataListenerBinding, BaseViewModel>(R.layout.fragment_data_listener) {
     override fun bindView() {
-
+        parentFragmentManager.setFragmentResultListener(
+            "key",
+            this,
+            FragmentResultListener { _, result ->
+                toast(result.getString("data"))
+            })
     }
 
     override fun bindData() {
--- KotlinLibraryX/app-api/src/main/java/com/lee/api/fragment/DataSendFragment.kt
+++ KotlinLibraryX/app-api/src/main/java/com/lee/api/fragment/DataSendFragment.kt
@@ -1,5 +1,6 @@
 package com.lee.api.fragment
 
+import android.os.Bundle
 import com.lee.api.R
 import com.lee.api.databinding.FragmentDataSendBinding
 import com.lee.library.base.BaseFragment
@@ -8,7 +9,11 @@ import com.lee.library.mvvm.base.BaseViewModel
 class DataSendFragment :
     BaseFragment<FragmentDataSendBinding, BaseViewModel>(R.layout.fragment_data_send) {
     override fun bindView() {
-
+        binding.btnSend.setOnClickListener {
+            parentFragmentManager.setFragmentResult("key", Bundle().apply {
+                putString("data", "this is fragment send data.")
+            })
+        }
     }
 
     override fun bindData() {
