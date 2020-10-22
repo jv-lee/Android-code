c60c19c2a2723bc2516e94aa6f6221bd61a2e7a2
jv.lee <jv.lee@foxmail.com>
2020-10-22 09:13:11 +0800

    添加api示例.

--- KotlinLibraryX/app-api/src/main/java/com/lee/api/activity/StartResultActivity.kt
+++ KotlinLibraryX/app-api/src/main/java/com/lee/api/activity/StartResultActivity.kt
@@ -1,26 +1,80 @@
 package com.lee.api.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.lee.api.R
 import com.lee.api.databinding.ActivityStartResultBinding
 import com.lee.library.base.BaseActivity
 import com.lee.library.mvvm.base.BaseViewModel
+import java.io.File
 
 class StartResultActivity :
     BaseActivity<ActivityStartResultBinding, BaseViewModel>(R.layout.activity_start_result) {
 
-    private val activityForResult = prepareCall(ActivityResultContracts.StartActivityForResult()) {
+    private val dataResult = prepareCall(ActivityResultContracts.StartActivityForResult()) {
         toast(it?.data?.getStringExtra("value") ?: "")
+    }
+
+    private val permissionsResult =
+        prepareCall(ActivityResultContracts.RequestPermissions()) { it ->
+            it ?: return@prepareCall
+            it.forEach {
+                toast("${it.key} request ${it.value}")
+            }
+        }
+
+    private val pictureResult = prepareCall(ActivityResultContracts.TakePicture()) {
+        binding.ivPicture.setImageBitmap(it)
     }
 
     override fun bindView() {
         binding.btnActivityResult.setOnClickListener {
-            activityForResult.launch(Intent(this, ResultActivity::class.java))
+            dataResult.launch(Intent(this, ResultActivity::class.java))
+        }
+        binding.btnPermissionsResult.setOnClickListener {
+            permissionsResult.launch(arrayOf(Manifest.permission.CAMERA))
+        }
+        binding.btnPictureResult.setOnClickListener {
+            //权限校验
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                permissionsResult.launch(arrayOf(Manifest.permission.CAMERA))
+                return@setOnClickListener
+            }
+
+            //设置文件路径创建文件对象
+            val path =
+//                filesDir.absolutePath + File.separator + "img" + File.separator + System.currentTimeMillis() + File.separator + ".jpg"
+//                filesDir.absolutePath + File.separator + System.currentTimeMillis() + File.separator + ".jpg"
+                Environment.getExternalStorageDirectory().absolutePath + File.separator + System.currentTimeMillis() + ".jpg"
+            val file = File(path)
+
+            //文件创建操作
+//            if (!file.parentFile.exists()) file.parentFile.mkdir()
+//            if (!file.exists()) file.createNewFile()
+
+            val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                FileProvider.getUriForFile(this, "$packageName.fileprovider", file)
+            } else {
+                Uri.fromFile(file)
+            }
+
+//            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+//            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+//            startActivityForResult(intent, 1)
+            pictureResult.launch(uri)
         }
     }
 
     override fun bindData() {
 
     }
+
 }
--- KotlinLibraryX/app-api/src/test/java/com/lee/api/ExampleUnitTest.kt
+++ KotlinLibraryX/app-api/src/test/java/com/lee/api/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package com.lee.api
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,11 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        val time = System.currentTimeMillis()
+        println(SimpleDateFormat("yyyy-MM-dd").format(Date(time)))
+        val time2 = time / 1000
+        val time3 = time2 * 1000
+        println(SimpleDateFormat("yyyy-MM-dd").format(Date(time3)))
+        println(SimpleDateFormat("yyyy-MM-dd").format(Date("100000000".toLong())))
     }
 }
