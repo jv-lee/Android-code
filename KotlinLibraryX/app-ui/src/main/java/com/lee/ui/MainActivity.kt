10d2bfbc99790bab3cad60b9743cd20df6b78418
jv.lee <jv.lee@foxmail.com>
2020-10-23 18:10:06 +0800

    添加ui示例.

--- KotlinLibraryX/app-ui/src/main/java/com/lee/ui/MainActivity.kt
+++ KotlinLibraryX/app-ui/src/main/java/com/lee/ui/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.lee.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.btn_selector_1).setOnClickListener { }
+        findViewById<TextView>(R.id.btn_selector_2).setOnClickListener { }
+        findViewById<TextView>(R.id.btn_selector_3).setOnClickListener { }
     }
+
+
 }
