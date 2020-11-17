f41f8f2ce60a4a366336ae4fed303548f99ff6ba
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-11-17 20:18:20 +0800

    剔除无用类.

--- EventCalendar/app/src/main/java/com/lee/calendar/entity/DateEntity.kt
+++ EventCalendar/app/src/main/java/com/lee/calendar/entity/DateEntity.kt
@@ -1,6 +1,8 @@
 package com.lee.calendar.entity
 
+import androidx.annotation.IntDef
 import com.lee.calendar.utils.CalendarUtils
+import com.lee.calendar.widget.MonthView
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -39,5 +41,34 @@ data class DateEntity(
 
     override fun toString(): String {
         return "{${year}-${month}-${dayList}}\n\n"
+    }
+}
+
+data class DayEntity(
+    val isToMonth: Boolean = true, //是否为当月数据
+    var isSelected: Boolean = false, //是否为选中状态
+    @DayStatus var dayStatus: Int = DayStatus.EMPTY_STATUS,
+    @MonthView.DayBackgroundStatus var backgroundStatus:Int = MonthView.DayBackgroundStatus.STATUS_GONE,
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val startIndex:Int,
+    val isToDay:Boolean = false
+){
+
+    override fun toString(): String {
+        return "{${year}-${month}-${day}}"
+    }
+}
+
+@IntDef(DayStatus.EMPTY_STATUS, DayStatus.UPDATE_STATUS, DayStatus.OVER_UPDATE_STATUS,DayStatus.DELAY_UPDATE_STATUS)
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class DayStatus {
+    companion object {
+        const val UPDATE_STATUS = 0 //0已更新
+        const val OVER_UPDATE_STATUS = 1 //1断更
+        const val EMPTY_STATUS = 2 //2无数据
+        const val DELAY_UPDATE_STATUS = 3 //3定时更新
     }
 }
