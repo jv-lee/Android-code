30f8dcc3dbdac9fa83524ce5bb02f516f43e9381
jv.lee <jv.lee@foxmail.com>
2020-11-24 10:27:55 +0800

    替换工具类.

---
+++ EventCalendar/app/src/main/java/com/lee/calendar/utils/AttendanceDataUtils.kt
@@ -0,0 +1,167 @@
+package com.lee.calendar.utils
+
+import com.lee.calendar.entity.DateData
+import com.lee.calendar.entity.DayStatus
+import com.lee.calendar.widget.MonthView
+import java.util.*
+
+/**
+ * @author jv.lee
+ * @date 2020/11/17
+ * @description
+ */
+object AttendanceDataUtils {
+
+    /**
+     * 是否使用缓存
+     * 小于当月 返回true
+     * 大于等于当月 返回false
+     */
+    fun hasCache(year: Int, month: Int): Boolean {
+        val calendar = Calendar.getInstance()
+        return if (calendar.get(Calendar.YEAR) == year && month < calendar.get(Calendar.MONTH)) {
+            true
+        } else calendar.get(Calendar.YEAR) < year
+    }
+
+    fun setBackgroundStatus(monthData: List<DateData>?, year: Int, month: Int, hashWeek: Boolean = false): List<DateData>? {
+        monthData ?: return null
+        for ((index, item) in monthData.withIndex()) {
+            paresBackgroundStatus(index, item, monthData, year, month, hashWeek)
+        }
+        return monthData
+    }
+
+    private fun paresBackgroundStatus(
+            position: Int,
+            dayEntity: DateData,
+            arrayList: List<DateData>,
+            year: Int,
+            month: Int,
+            hashWeek: Boolean
+    ) {
+        val isDrawGone = isDrawGone(dayEntity)
+        if (isDrawGone) {
+            dayEntity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_GONE
+            return
+        }
+
+        val isDrawSingle = isDrawSingle(position, dayEntity, arrayList)
+        if (isDrawSingle) {
+            dayEntity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
+            return
+        }
+
+        if (hashWeek) {
+            isDrawWeekLocation(position, dayEntity, arrayList, year, month)
+        } else {
+            isDrawLocation(position, dayEntity, arrayList, year, month)
+        }
+    }
+
+    private fun isDrawGone(entity: DateData): Boolean {
+        return entity.status != DayStatus.UPDATE_STATUS && entity.status != DayStatus.OVER_UPDATE_STATUS
+    }
+
+    private fun isDrawSingle(
+            position: Int,
+            entity: DateData,
+            arrayList: List<DateData>
+    ): Boolean {
+        val prevEntity: DateData? = if (position == 0) null else arrayList[position - 1]
+        val nextEntity: DateData? =
+                if (position == arrayList.size - 1) null else arrayList[position + 1]
+        if (prevEntity == null) {
+            return entity.status != nextEntity?.status
+        }
+        if (nextEntity == null) {
+            return entity.status != prevEntity.status
+        }
+        return entity.status != prevEntity.status && entity.status != nextEntity.status
+    }
+
+    private fun isDrawLocation(position: Int, entity: DateData, arrayList: List<DateData>, year: Int, month: Int) {
+        val prevEntity: DateData? = if (position == 0) null else arrayList[position - 1]
+        val nextEntity: DateData? =
+                if (position == arrayList.size - 1) null else arrayList[position + 1]
+        val week = CalendarUtils.getDayOfWeek(year, month, entity.day)
+
+        //当月第一天
+        if (prevEntity == null && entity.status == nextEntity?.status && week != 7) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
+            return
+        }
+
+        //当月最后一天
+        if (nextEntity == null && entity.status == prevEntity?.status && week != 1) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
+            return
+        }
+
+        if (entity.status != prevEntity?.status && entity.status == nextEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
+        }
+
+        if (entity.status == prevEntity?.status && entity.status == nextEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_CENTER
+        }
+
+        if (entity.status == prevEntity?.status && entity.status != nextEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
+        }
+
+        //周天 / 周六切割
+        if (week == 1 && entity.status == nextEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
+        }
+        if (week == 1 && entity.status != nextEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
+        }
+        if (week == 7 && entity.status == prevEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
+        }
+        if (week == 7 && entity.status != prevEntity?.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
+        }
+    }
+
+    private fun isDrawWeekLocation(position: Int, entity: DateData, arrayList: List<DateData>, year: Int, month: Int) {
+        val prevEntity: DateData? = if (position == 0) null else arrayList[position - 1]
+        val nextEntity: DateData? =
+                if (position == arrayList.size - 1) null else arrayList[position + 1]
+
+        //星期天
+        if (prevEntity == null) {
+            if (entity.status != nextEntity?.status) {
+                entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
+            } else if (entity.status == nextEntity.status) {
+                entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
+            }
+            return
+        }
+
+        //星期六
+        if (nextEntity == null) {
+            if (entity.status != prevEntity.status) {
+                entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
+            } else if (entity.status == prevEntity.status) {
+                entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
+            }
+            return
+        }
+
+        //周内
+        if (entity.status != prevEntity.status && entity.status == nextEntity.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
+        }
+
+        if (entity.status == prevEntity.status && entity.status == nextEntity.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_CENTER
+        }
+
+        if (entity.status == prevEntity.status && entity.status != nextEntity.status) {
+            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
+        }
+
+    }
+}
--- EventCalendar/app/src/main/java/com/lee/calendar/viewmodel/TestViewModel.kt
+++ EventCalendar/app/src/main/java/com/lee/calendar/viewmodel/TestViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lee.calendar.entity.DateData
 import com.lee.calendar.entity.DayStatus
+import com.lee.calendar.utils.AttendanceDataUtils
 import com.lee.calendar.utils.CalendarUtils
-import com.lee.calendar.widget.MonthView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeUnit
  */
 class TestViewModel : ViewModel() {
 
-    data class DateObserver(val position: Int, val data: ArrayList<DateData>)
+    data class DateObserver(val position: Int, val data: List<DateData>)
 
     val monthLiveData by lazy { MutableLiveData<DateObserver>() }
     val weekLiveData by lazy { MutableLiveData<DateObserver>() }
@@ -27,7 +27,7 @@ class TestViewModel : ViewModel() {
         val disposable = Observable.create<ArrayList<DateData>> {
             it.onNext(createMonthData(year, month))
         }.map {
-            setBackgroundStatus(it,year,month)
+            AttendanceDataUtils.setBackgroundStatus(it,year,month)
         }.map {
             DateObserver(position, it)
         }.delay(500,TimeUnit.MILLISECONDS)
@@ -42,7 +42,7 @@ class TestViewModel : ViewModel() {
         val disposable = Observable.create<ArrayList<DateData>> {
             it.onNext(createWeekData(year, month,day))
         }.map {
-            setBackgroundStatus(it,year,month)
+            AttendanceDataUtils.setBackgroundStatus(it,year,month)
         }.map {
             DateObserver(position, it)
         }.delay(500,TimeUnit.MILLISECONDS)
@@ -106,101 +106,6 @@ class TestViewModel : ViewModel() {
             }
         }
         return monthArray
-    }
-
-    private fun setBackgroundStatus(monthData: ArrayList<DateData>, year: Int, month: Int): ArrayList<DateData> {
-        for ((index, item) in monthData.withIndex()) {
-            paresBackgroundStatus(index,item,monthData,year,month)
-        }
-        return monthData
-    }
-
-    private fun paresBackgroundStatus(
-        position: Int,
-        dayEntity: DateData,
-        arrayList: ArrayList<DateData>,
-        year:Int,
-        month:Int
-    ) {
-        val isDrawGone = isDrawGone(dayEntity)
-        if (isDrawGone) {
-            dayEntity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_GONE
-            return
-        }
-
-        val isDrawSingle = isDrawSingle(position, dayEntity, arrayList)
-        if (isDrawSingle) {
-            dayEntity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
-            return
-        }
-
-        isDrawLocation(position, dayEntity, arrayList,year,month)
-    }
-
-    private fun isDrawGone(entity: DateData): Boolean {
-        return entity.status != DayStatus.UPDATE_STATUS && entity.status != DayStatus.OVER_UPDATE_STATUS
-    }
-
-    private fun isDrawSingle(
-        position: Int,
-        entity: DateData,
-        arrayList: ArrayList<DateData>
-    ): Boolean {
-        val prevEntity: DateData? = if (position == 0) null else arrayList[position - 1]
-        val nextEntity: DateData? =
-            if (position == arrayList.size - 1) null else arrayList[position + 1]
-        if (prevEntity == null) {
-            return entity.status != nextEntity?.status
-        }
-        if (nextEntity == null) {
-            return entity.status != prevEntity.status
-        }
-        return entity.status != prevEntity.status && entity.status != nextEntity.status
-    }
-
-    private fun isDrawLocation(position: Int, entity: DateData, arrayList: ArrayList<DateData>, year:Int, month: Int) {
-        val prevEntity: DateData? = if (position == 0) null else arrayList[position - 1]
-        val nextEntity: DateData? =
-            if (position == arrayList.size - 1) null else arrayList[position + 1]
-
-        //当月第一天
-        if (prevEntity == null && entity.status == nextEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
-            return
-        }
-
-        //当月最后一天
-        if (nextEntity == null && entity.status == prevEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
-            return
-        }
-
-        if (entity.status != prevEntity?.status && entity.status == nextEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
-        }
-
-        if (entity.status == prevEntity?.status && entity.status == nextEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_CENTER
-        }
-
-        if (entity.status == prevEntity?.status && entity.status != nextEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
-        }
-
-        //周天 / 周六切割
-        val week = CalendarUtils.getDayOfWeek(year, month, entity.day)
-        if (week == 1 && entity.status == nextEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_START
-        }
-        if (week == 1 && entity.status != nextEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
-        }
-        if (week == 7 && entity.status == prevEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_END
-        }
-        if (week == 7 && entity.status != prevEntity?.status) {
-            entity.backgroundStatus = MonthView.DayBackgroundStatus.STATUS_SINGLE
-        }
     }
 
 }
