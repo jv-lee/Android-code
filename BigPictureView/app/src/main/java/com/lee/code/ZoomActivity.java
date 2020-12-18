e205aacd35fd140fda680484a761aaa709328cd9
jv.lee <jv.lee@foxmail.com>
2020-12-18 17:55:55 +0800

    修改ZoomImageView 更加平滑.

---
+++ BigPictureView/app/src/main/java/com/lee/code/ZoomActivity.java
@@ -0,0 +1,19 @@
+package com.lee.code;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author jv.lee
+ * @date 2020/12/17
+ * @description
+ */
+public class ZoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoom);
+    }
+}
