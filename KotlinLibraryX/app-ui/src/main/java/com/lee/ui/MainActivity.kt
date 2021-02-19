067d47d82475086b177e1a00c7068ed945b704cc
jv.lee <jv.lee@foxmail.com>
2021-02-19 16:51:09 +0800

    修改代码格式.

--- KotlinLibraryX/app-ui/src/main/java/com/lee/ui/MainActivity.kt
+++ KotlinLibraryX/app-ui/src/main/java/com/lee/ui/MainActivity.kt
@@ -9,26 +9,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val vpAdapter by lazy {
-        UiPagerAdapter(
-            supportFragmentManager,
-            fragments,
-            titles
-        )
-    }
+    private val vpAdapter by lazy { UiPagerAdapter(supportFragmentManager, fragments, titles) }
     private val fragments by lazy { listOf(SelectorFragment(), WheelFragment(), ShadowFragment()) }
-    private val titles by lazy {
-        listOf(
-            getString(R.string.nav_selector),
-            getString(R.string.nav_wheel),
-            getString(R.string.nav_shadow)
-        )
-    }
+    private val titles by lazy { listOf(getString(R.string.nav_selector), getString(R.string.nav_wheel), getString(R.string.nav_shadow)) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         vp_container.adapter = vpAdapter
         vp_container.setNoScroll(true)
+
         bottom_nav.bindViewPager(vp_container)
         bottom_nav.setDotVisibility(0, View.VISIBLE)
         bottom_nav.setNumberDot(1, 999)
