af3dac5eab4511e3d184f4cd54b0c113af3d2054
jv.lee <jv.lee@foxmail.com>
2021-02-24 17:19:26 +0800

    添加WorkerManager任务类.

---
+++ KotlinLibraryX/app-api/src/main/java/com/lee/api/worker/TaskWorker.kt
@@ -0,0 +1,30 @@
+package com.lee.api.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+/**
+ * @author jv.lee
+ * @date 2021/2/24
+ * @description workManager 任务类
+ */
+class TaskWorker(context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+
+    private val TAG: String = TaskWorker::class.java.simpleName
+
+    override fun doWork(): Result {
+        val code = 1
+        Log.i(TAG, "doWork: task ++ ")
+
+        return when (code) {
+            1 -> Result.success()
+            2 -> Result.failure()
+            3 -> Result.retry()
+            else -> return Result.success()
+        }
+    }
+
+}
