c303018957f98cf2fe0dad8e0eb9444fc81f5551
lijiawei11 <lijiawei11@baidu.com>
2021-10-20 16:05:59 +0800

    update compose code.

--- KotlinLibraryX/app-compose/build.gradle.kts
+++ KotlinLibraryX/app-compose/build.gradle.kts
@@ -10,9 +10,12 @@ appConfigure(packageName = "com.simple.compose", projectConfigure = {
         implementation("androidx.compose.ui:ui:$composeVersion")
         implementation("androidx.compose.material:material:$composeVersion")
         implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-        implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+        implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
 
-        testImplementation("junit:junit:4.+")
+        implementation("androidx.activity:activity-compose:1.3.0")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+        implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-beta01")
+
         androidTestImplementation("androidx.test.ext:junit:1.1.2")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
         androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
---
+++ KotlinLibraryX/app-compose/src/main/java/com/simple/compose/ComposeStateActivity.kt
@@ -0,0 +1,95 @@
+package com.simple.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * @author jv.lee
+ * @data 2021/10/20
+ * @description
+ */
+class ComposeStateActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloScreen()
+        }
+    }
+
+    class HelloViewModel : ViewModel() {
+        private val _name = MutableLiveData("")
+        val name: LiveData<String> = _name
+
+        fun onNameChange(newName: String) {
+            _name.value = newName
+        }
+
+        private val _email = MutableStateFlow("")
+        val email: StateFlow<String> = _email
+
+        fun onEmailChange(newEmail: String) {
+            _email.tryEmit(newEmail)
+        }
+    }
+
+    @Composable
+    fun HelloScreen(helloViewModel: HelloViewModel = viewModel()) {
+        val name: String by helloViewModel.name.observeAsState("")
+        val email: String by helloViewModel.email.collectAsState("")
+        HelloContent(
+            name = name, onNameChange = { helloViewModel.onNameChange(it) },
+            email = email, onEmailChange = { helloViewModel.onEmailChange(it) }
+        )
+    }
+
+    @Composable
+    fun HelloContent(
+        name: String, onNameChange: (String) -> Unit,
+        email: String, onEmailChange: (String) -> Unit
+    ) {
+        Surface(color = Color.White) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                if (name.isNotEmpty()) {
+                    Text(
+                        text = "Hello, $name! email:$email.",
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        style = MaterialTheme.typography.h5
+                    )
+                }
+
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = onNameChange,
+                    label = { Text(text = ("Name")) })
+
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = onEmailChange,
+                    label = { Text(text = ("Email")) })
+            }
+        }
+
+    }
+
+}
