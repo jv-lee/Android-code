d2123d5b83c002c8d3fa2df0cdc8d84b54736c33
lijiawei11 <lijiawei11@baidu.com>
2021-10-25 10:40:23 +0800

    update cacheManager.

--- KotlinLibraryX/library/src/main/java/com/lee/library/extensions/CacheExtensions.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/extensions/CacheExtensions.kt
@@ -7,21 +7,22 @@ import kotlinx.coroutines.CompletableDeferred
 /**
  * @author jv.lee
  * @date 2021/9/9
- * @description
+ * @description 缓存管理器扩展类
  */
+
 /**
  * @param key 缓存key
  */
-suspend inline fun <reified T> CacheManager.getCache(key: String): T? {
+inline fun <reified T> CacheManager.getCache(key: String): T? {
     val type = object : TypeToken<T>() {}.type
-    return CompletableDeferred(CacheManager.getDefault().get<T>(key, type)).await()
+    return get<T>(key, type)
 }
 
 /**
  * @param key 存储key
  * @param data 存储数据源
  */
-fun <T> CacheManager.putCache(key: String, data: T) {
-    CacheManager.getDefault().put(key, data)
+inline fun <reified T> CacheManager.putCache(key: String, data: T) {
+    put(key, data)
 }
 
