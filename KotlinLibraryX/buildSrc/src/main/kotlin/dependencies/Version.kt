edc88d9edc713d5fad79247e5ced997a1a32f394
lijiawei11 <lijiawei11@baidu.com>
2021-10-29 19:29:36 +0800

    update dependency version.

--- KotlinLibraryX/buildSrc/src/main/kotlin/dependencies/Version.kt
+++ KotlinLibraryX/buildSrc/src/main/kotlin/dependencies/Version.kt
@@ -3,7 +3,7 @@ package dependencies
 object Version {
 
     //core
-    const val ktxCore = "1.2.0"
+    const val ktxCore = "1.6.0"
     const val coroutines = "1.3.9"
     const val multidex = "2.0.1"
     const val appcompat = "1.3.1"
@@ -18,7 +18,7 @@ object Version {
     const val lifecycle = "2.4.0-alpha03"
     const val room = "2.3.0"
     const val glide = "4.11.0"
-    const val retrofit = "2.9.0"
+    const val retrofit = "2.6.0"
     const val gson = "2.8.8"
     const val protobuf = "3.10.0"
     const val autoService = "1.0"
