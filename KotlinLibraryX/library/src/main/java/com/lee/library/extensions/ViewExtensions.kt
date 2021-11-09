2fd56a46fbb90d758bde05388e28727d1c4c8e91
lijiawei11 <lijiawei11@baidu.com>
2021-11-09 11:01:04 +0800

    update ViewExtensions.kt

--- KotlinLibraryX/library/src/main/java/com/lee/library/extensions/ViewExtensions.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/extensions/ViewExtensions.kt
@@ -15,6 +15,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.lee.library.R
 import com.lee.library.tools.StatusTools
@@ -80,7 +81,7 @@ fun View.setBackgroundSelectorTintCompat(selectorId: Int) {
         this,
         ColorStateList.createFromXml(
             this.resources,
-            this.resources.getXml(selectorId),
+            this.resources.getXml(selectorId)
         )
     )
 }
@@ -347,8 +348,8 @@ fun EditText.setBankCodeTextWatcher(lengthLimit: Int = 16) {
             }
         }
 
-        private fun addSpaceByCredit(contents: String, lengthLimit: Int): String {
-            var content = contents
+        private fun addSpaceByCredit(content: String, lengthLimit: Int): String {
+            var content = content
             if (TextUtils.isEmpty(content)) {
                 return ""
             }
@@ -486,5 +487,25 @@ fun WebView.setWebBackEvent() {
  */
 fun RadioGroup.checkUnNotification(@IdRes id: Int) {
     findViewById<RadioButton>(id).isChecked = true
+}
+
+/**
+ * 更具滑动page，慢慢递增page缓存
+ * 解决一次性缓存所有page页面初始化卡顿问题
+ */
+fun ViewPager2.increaseOffscreenPageLimit() {
+    registerOnPageChangeCallback(object :
+        ViewPager2.OnPageChangeCallback() {
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+            if (position == 0) return
+            if (offscreenPageLimit <= position) {
+                offscreenPageLimit = position
+            }
+        }
+    })
 }
 
