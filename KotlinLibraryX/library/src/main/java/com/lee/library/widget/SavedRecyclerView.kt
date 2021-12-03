11351db7c73c255b23744d5c89de4777219a8d3f
lijiawei11 <lijiawei11@baidu.com>
2021-12-03 10:48:13 +0800

    update SavedRecyclerView.kt

--- KotlinLibraryX/library/src/main/java/com/lee/library/widget/SavedRecyclerView.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/widget/SavedRecyclerView.kt
@@ -19,6 +19,10 @@ import androidx.recyclerview.widget.RecyclerView
  * 这种情况RecyclerView只会保存恢复自身状态 不会处理子View状态保存恢复
  */
 class SavedRecyclerView : RecyclerView {
+
+    //存储需要被记录状态的view类型 根据recycler adapter 不同item类型，需要保存的存储类型
+    private val stateTypes = HashSet<Class<*>>()
+
     constructor(context: Context) : super(context, null, 0)
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet, 0)
     constructor(context: Context, attributeSet: AttributeSet, defStyle: Int) : super(
@@ -29,18 +33,29 @@ class SavedRecyclerView : RecyclerView {
 
     override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>?) {
         super.dispatchSaveInstanceState(container)
-        children.forEach {
-            it.saveHierarchyState(container)
+        children.forEach { view ->
+            if (stateTypes.contains(view.javaClass)) {
+                view.saveHierarchyState(container)
+            }
         }
     }
 
     override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>?) {
         super.dispatchRestoreInstanceState(container)
         post {
-            children.forEach {
-                it.restoreHierarchyState(container)
+            children.forEach { view ->
+                if (stateTypes.contains(view.javaClass)) {
+                    view.restoreHierarchyState(container)
+                }
             }
         }
+    }
+
+    /**
+     * 添加需要记录状态的view类型
+     */
+    fun addSaveStateViewType(type: Class<*>) {
+        stateTypes.add(type)
     }
 
 }
