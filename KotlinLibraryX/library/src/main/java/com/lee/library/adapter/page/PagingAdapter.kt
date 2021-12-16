fb080cf4baec19219fada6657b677d9ae2d80884
lijiawei11 <lijiawei11@baidu.com>
2021-12-16 19:40:03 +0800

    update PagingAdapter.kt

--- KotlinLibraryX/library/src/main/java/com/lee/library/adapter/page/PagingAdapter.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/adapter/page/PagingAdapter.kt
@@ -16,12 +16,6 @@ fun <T> BaseViewAdapter<T>.submitData(
     refreshBlock: () -> Unit = {},
     emptyBlock: () -> Unit = {}
 ) {
-    if (data.isEmpty() && pageData.getDataSource().isNullOrEmpty()) {
-        pageEmpty()
-        emptyBlock()
-        return
-    }
-
     //首页加载逻辑
     if (pageData.getPageNumber() == limit) {
         // 过滤首页重复数据
@@ -77,12 +71,6 @@ fun <T> BaseViewAdapter<T>.submitData(
     refreshBlock: () -> Unit = {},
     emptyBlock: () -> Unit = {}
 ) {
-    if (data.isEmpty() && pageData.getDataSource().isNullOrEmpty()) {
-        pageEmpty()
-        emptyBlock()
-        return
-    }
-
     //首页加载逻辑
     if (pageData.isFirstPage()) {
         // 过滤首页重复数据
