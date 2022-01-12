0a4bc66d9177003e9b121e81c9eb80092321e380
lijiawei11 <lijiawei11@baidu.com>
2022-01-12 18:11:17 +0800

    update file name.

--- KotlinLibraryX/library/src/main/java/com/lee/library/adapter/core/UiPagerAdapter2.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/adapter/core/UiPagerAdapter2.kt
@@ -8,7 +8,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
  * @author jv.lee
  * description：PageFragment适配器 -> 适用于ViewPager2
  */
-class UiPager2Adapter : FragmentStateAdapter {
+class UiPagerAdapter2 : FragmentStateAdapter {
     private val fragmentList: MutableList<Fragment>
 
     constructor(fragmentActivity: FragmentActivity, fragments: MutableList<Fragment>)
