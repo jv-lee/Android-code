b1f95eefab82795f27edaf98f0e7a1f0a021d2f1
lijiawei11 <lijiawei11@baidu.com>
2022-01-13 15:04:40 +0800

    add NewGoogleViewActivity.kt

---
+++ KotlinLibraryX/app-ui/src/main/java/com/lee/ui/NewGoogleViewActivity.kt
@@ -0,0 +1,23 @@
+package com.lee.ui
+
+import com.lee.library.base.BaseActivity
+import com.lee.library.extensions.binding
+import com.lee.ui.databinding.ActivityNewGoogleViewBinding
+
+/**
+ * @author jv.lee
+ * @date 2022/1/13
+ * @description
+ */
+class NewGoogleViewActivity : BaseActivity() {
+
+    private val binding by binding(ActivityNewGoogleViewBinding::inflate)
+
+    override fun bindView() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
