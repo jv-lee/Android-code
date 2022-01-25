20642715d6db55e5efb5351116d625bf151ae5c2
lijiawei11 <lijiawei11@baidu.com>
2022-01-25 14:49:08 +0800

    update UiPagerAdapter2.kt

--- KotlinLibraryX/library/src/main/java/com/lee/library/adapter/core/UiPagerAdapter2.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/adapter/core/UiPagerAdapter2.kt
@@ -1,24 +1,18 @@
 package com.lee.library.adapter.core
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 /**
  * @author jv.lee
  * description：PageFragment适配器 -> 适用于ViewPager2
  */
-class UiPagerAdapter2 : FragmentStateAdapter {
-    private val fragmentList: MutableList<Fragment>
+class UiPagerAdapter2(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
 
-    constructor(fragmentActivity: FragmentActivity, fragments: MutableList<Fragment>)
-            : super(fragmentActivity) {
-        fragmentList = fragments
-    }
-
-    constructor(fragment: Fragment, fragments: MutableList<Fragment>) : super(fragment) {
-        fragmentList = fragments
-    }
+    private val fragmentList = ArrayList<Fragment>()
 
     override fun createFragment(position: Int): Fragment {
         return fragmentList[position]
@@ -29,5 +23,14 @@ class UiPagerAdapter2 : FragmentStateAdapter {
     }
 
     fun getFragments() = fragmentList
+
+    fun addAll(fragments: MutableList<Fragment>) {
+        clear()
+        fragmentList.addAll(fragments)
+    }
+
+    fun clear() {
+        fragmentList.clear()
+    }
 
 }
