c09b9cafa11164ce39f8a4572e1c4cffea031fca
lijiawei11 <lijiawei11@baidu.com>
2022-02-14 17:43:59 +0800

    update PagingAdapter.kt add.

--- KotlinLibraryX/library/src/main/java/com/lee/library/adapter/page/PagingAdapter.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/adapter/page/PagingAdapter.kt
@@ -22,6 +22,8 @@ fun <T> BaseViewAdapter<T>.submitData(
         if (data == pageData.getDataSource()) {
             // 重复数据空数据校验
             if (pageData.getDataSource().isNullOrEmpty()) {
+                if (isPageCompleted) initStatusView()
+                clearData()
                 pageEmpty()
                 emptyBlock()
             }
@@ -30,6 +32,8 @@ fun <T> BaseViewAdapter<T>.submitData(
 
         //设置空页面
         if (pageData.getDataSource().isNullOrEmpty()) {
+            if (isPageCompleted) initStatusView()
+            clearData()
             pageEmpty()
             emptyBlock()
             return
@@ -45,10 +49,17 @@ fun <T> BaseViewAdapter<T>.submitData(
 
         //分页加载逻辑
     } else {
+
+        // 数据相同不处理
+        if (data == pageData.getDataSource()) {
+            return
+        }
+
         //防止view重构后在分页加载时 pageCompleted状态重置
         if (!isPageCompleted) {
             pageCompleted()
         }
+
         if (diff) {
             //防止activity重建在viewModel中填充历史数据 做差分填充
             val oldData = data
@@ -105,10 +116,17 @@ fun <T> BaseViewAdapter<T>.submitData(
 
         //分页加载逻辑
     } else {
+
+        // 数据相同不处理
+        if (data == pageData.getDataSource()) {
+            return
+        }
+
         //防止view重构后在分页加载时 pageCompleted状态重置
         if (!isPageCompleted) {
             pageCompleted()
         }
+
         if (diff) {
             //防止activity重建在viewModel中填充历史数据 做差分填充
             val oldData = data
