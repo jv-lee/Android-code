1e59fe6be29f7528834ce9069f1afaf764ae7eb2
lijiawei11 <lijiawei11@baidu.com>
2022-04-07 15:16:30 +0800

    update BaseNavigationFragment.kt

--- KotlinLibraryX/library/src/main/java/com/lee/library/base/BaseNavigationFragment.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/base/BaseNavigationFragment.kt
@@ -3,6 +3,7 @@ package com.lee.library.base
 import androidx.annotation.NonNull
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleRegistry
+import androidx.navigation.fragment.findNavController
 
 /**
  * @author jv.lee
@@ -11,18 +12,81 @@ import androidx.lifecycle.LifecycleRegistry
  */
 abstract class BaseNavigationFragment(val layoutId: Int) : BaseFragment(layoutId) {
 
+    private var isResume = false
+    private var isStop = true
+
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
         if (hidden) {
             onPause()
             onStop()
-            handleViewLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-            handleViewLifecycleEvent(Lifecycle.Event.ON_STOP)
+            handleFragmentStop()
         } else {
             onStart()
             onResume()
-            handleViewLifecycleEvent(Lifecycle.Event.ON_START)
-            handleViewLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            handleFragmentResume()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val currentClass = this::class.java.simpleName
+        val destination: String
+        val parentClass: String
+
+        if (parentFragment != null) {
+            destination =
+                requireParentFragment().findNavController().currentDestination.toString()
+            parentClass = requireParentFragment()::class.java.simpleName
+        } else {
+            destination = findNavController().currentDestination.toString()
+            parentClass = ""
+        }
+
+        if (destination.contains(currentClass) || destination.contains(parentClass)) {
+            handleFragmentResume()
+        } else {
+            postFragmentStop()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handleFragmentStop()
+    }
+
+    open fun onFragmentResume() {
+        isResume = true
+        isStop = false
+        handleViewLifecycleEvent(Lifecycle.Event.ON_START)
+        handleViewLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+
+    open fun onFragmentStop() {
+        isResume = false
+        isStop = true
+    }
+
+
+    private fun handleFragmentResume() {
+        if (!isResume) {
+            onFragmentResume()
+        }
+    }
+
+    private fun handleFragmentStop() {
+        handleViewLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        handleViewLifecycleEvent(Lifecycle.Event.ON_STOP)
+        if (!isStop) {
+            onFragmentStop()
+        }
+    }
+
+    private fun postFragmentStop() {
+        postViewLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        postViewLifecycleEvent(Lifecycle.Event.ON_STOP)
+        if (!isStop) {
+            onFragmentStop()
         }
     }
 
@@ -32,6 +96,11 @@ abstract class BaseNavigationFragment(val layoutId: Int) : BaseFragment(layoutId
 
     private fun handleViewLifecycleEvent(@NonNull event: Lifecycle.Event) {
         getViewLifecycleRegistry()?.handleLifecycleEvent(event)
+    }
+
+    private fun postViewLifecycleEvent(@NonNull event: Lifecycle.Event) {
+        val lifecycle = getViewLifecycleRegistry()
+        view?.post { lifecycle?.handleLifecycleEvent(event) }
     }
 
 }
