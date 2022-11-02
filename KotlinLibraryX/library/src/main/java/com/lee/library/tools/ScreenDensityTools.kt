ea923ca6b22d02714c8ebf90d255875d50ea17aa
lijiawei11 <lijiawei11@baidu.com>
2022-11-02 10:12:18 +0800

    update ScreenDensityTools.kt code.

--- KotlinLibraryX/library/src/main/java/com/lee/library/tools/ScreenDensityTools.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/tools/ScreenDensityTools.kt
@@ -4,23 +4,19 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
 import android.content.ComponentCallbacks
-import android.content.Context
 import android.content.res.Configuration
-import android.graphics.Point
-import android.os.Build
 import android.util.DisplayMetrics
-import android.view.Display
-import android.view.View
-import android.view.WindowManager
-import com.lee.library.utils.LogUtil
-import java.util.*
+import com.lee.library.extensions.height
+import com.lee.library.extensions.isLandscape
+import com.lee.library.extensions.isPortrait
+import com.lee.library.extensions.width
 
 /**
  * 屏幕适配帮助类
  * @author jv.lee
  * @date 2021/9/2
  */
-object ScreenDensityUtil : ComponentCallbacks {
+object ScreenDensityTools : ComponentCallbacks {
 
     private const val WIDTH = 360f
     var scale = 0f                                  //屏幕缩放倍数
@@ -28,22 +24,20 @@ object ScreenDensityUtil : ComponentCallbacks {
     private var scaledDensity: Float = 0f           //
     private lateinit var metrics: DisplayMetrics    //Application的Density
 
-    private val mComponentCallbacks: HashMap<String, ComponentCallbacks> = hashMapOf()
-
     /**
      * 在Application中初始化Metrics
      */
     fun init(application: Application) {
-        screenApp = application
-        //获取application的DisplayMetrics
-        metrics = application.resources.displayMetrics
-        //判断是否需要初始化
+        // 判断是否需要初始化
         if (density != 0f) return
-        //初始化
+
+        // 初始化
+        metrics = application.resources.displayMetrics
         density = metrics.density
         scaledDensity = metrics.scaledDensity
-        //监听字体变化
-        screenApp.registerComponentCallbacks(this)
+
+        // 监听字体变化
+        application.registerComponentCallbacks(this)
     }
 
     /**
@@ -55,36 +49,13 @@ object ScreenDensityUtil : ComponentCallbacks {
         ruler: Float = WIDTH,
         @Density flag: Int = Density.SHORT_SIDE_BASED
     ) {
-        val f = activity.window.decorView.systemUiVisibility
-        val sf1 = View.SYSTEM_UI_FLAG_FULLSCREEN
-        val sf2 = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        val nf = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        val sf = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        val navigation = ((f and sf) == sf) || ((f and nf) == nf)
-        val status = ((f and sf) == sf) || ((f and sf1) == sf1) || ((f and sf2) == sf2)
-        init(activity, ruler, status, navigation, flag)
-    }
-
-    /**
-     * 在Activity中初始化Density
-     * @param ruler UI的设计宽度，默认为420dp
-     * @param status 设置基准尺寸时是否包含标题栏
-     * @param navigation 设置基准尺寸时是否包含虚拟键
-     */
-    fun init(
-        activity: Activity,
-        ruler: Float = WIDTH,
-        status: Boolean = false,
-        navigation: Boolean = false,
-        @Density flag: Int = Density.SHORT_SIDE_BASED
-    ) {
-        val width = width(navigation)
-        val height = height(status, navigation)
+        val width = width()
+        val height = height()
         val pixels = when (flag) {
             Density.WIDTH_BASED -> width
             Density.HEIGHT_BASED -> height
-            Density.SHORT_SIDE_BASED -> if (activity.application.isPortrait) width else height
-            Density.LONG_SIDE_BASED -> if (activity.application.isLandscape) width else height
+            Density.SHORT_SIDE_BASED -> if (activity.isPortrait) width else height
+            Density.LONG_SIDE_BASED -> if (activity.isLandscape) width else height
             else -> 0
         }
         init(activity, pixels / ruler)
@@ -96,27 +67,8 @@ object ScreenDensityUtil : ComponentCallbacks {
      * @param landRuler UI横屏时的设计宽度，默认为980dp
      */
     fun init(activity: Activity, portRuler: Float = WIDTH, landRuler: Float) {
-        val f = activity.window.decorView.systemUiVisibility
-        val nf = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        val sf = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        val navigation = ((f and sf) == sf) || ((f and nf) == nf)
-        init(activity, portRuler, landRuler, navigation)
-    }
-
-    /**
-     * 在Activity中初始化Density
-     * @param portRuler UI竖屏时的设计宽度，默认为420dp
-     * @param landRuler UI横屏时的设计宽度，默认为980dp
-     * @param navigation 设置基准尺寸时是否包含虚拟键
-     */
-    fun init(
-        activity: Activity,
-        portRuler: Float = WIDTH,
-        landRuler: Float,
-        navigation: Boolean = false
-    ) {
-        val pixels = width(navigation)
-        val ruler = if (screenApp.isLandscape) landRuler else portRuler
+        val pixels = width()
+        val ruler = if (activity.isLandscape) landRuler else portRuler
         init(activity, pixels / ruler)
     }
 
@@ -126,9 +78,8 @@ object ScreenDensityUtil : ComponentCallbacks {
     private fun init(activity: Activity, density: Float) {
         activity.resources.displayMetrics.density = density
         activity.resources.displayMetrics.densityDpi = (160 * density).toInt()
-        activity.resources.displayMetrics.scaledDensity =
-            density * (scaledDensity / ScreenDensityUtil.density)
-        scale = ScreenDensityUtil.density / density
+        activity.resources.displayMetrics.scaledDensity = density * (scaledDensity / this.density)
+        scale = this.density / density
 
         setBitmapDensity(activity.resources.displayMetrics.densityDpi)
     }
@@ -174,11 +125,9 @@ object ScreenDensityUtil : ComponentCallbacks {
      * 字体变化时的回调
      */
     override fun onConfigurationChanged(newConfig: Configuration) {
-        LogUtil.i("onConfigurationChanged")
-        //字体改变后,将appScaledDensity重新赋值
+        // 字体改变后,将appScaledDensity重新赋值
         if (newConfig.fontScale > 0) scaledDensity = metrics.scaledDensity
     }
-
 
 }
 
@@ -189,83 +138,4 @@ annotation class Density {
         var LONG_SIDE_BASED = 3
         var SHORT_SIDE_BASED = 4
     }
-}
-
-private lateinit var screenApp: Application
-
-/**
- * 判断设备是否处于竖屏状态
- * Check if the device orientation is portrait.
- */
-private val Application.isPortrait get() = this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
-
-/**
- * 判断设备是否处于横屏状态
- * Check if the device orientation is landscape.
- */
-private val Application.isLandscape get() = this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-
-private val Application.windowManager get() = this.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
-private fun getPixel(id: Int) = screenApp.resources.getDimensionPixelSize(id)
-
-private val status: Int
-    @SuppressLint("InternalInsetResource", "DiscouragedApi")
-    get() {
-        val id = screenApp.resources.getIdentifier("status_bar_height", "dimen", "android")
-        return if (id > 0) getPixel(id) else 0
-    }
-
-private fun width(navigation: Boolean): Int = if (navigation) Screen().width else Display().width
-
-private fun height(hasStatus: Boolean, navigation: Boolean): Int {
-    val screen = Screen()
-    val display = Display()
-    return when {
-        hasStatus && navigation -> screen.height
-        navigation -> screen.height - status
-        hasStatus -> display.height()
-        else -> display.height
-    }
-}
-
-internal data class Screen(private val display: Display = screenApp.windowManager.defaultDisplay) {
-
-    @SuppressLint("ObsoleteSdkInt")
-    private val point = Point().apply {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            display.getRealSize(this)
-        } else {
-            display.getSize(this)
-        }
-    }
-
-    /**
-     * 屏幕宽度，包含虚拟键，单位px
-     */
-    val width: Int = point.x
-
-    /**
-     * 屏幕高度，包含虚拟键和状态栏，单位px
-     */
-    val height: Int = point.y
-}
-
-internal data class Display(private val metrics: DisplayMetrics = screenApp.resources.displayMetrics) {
-
-    /**
-     * 展示宽度，不包含虚拟键，单位px
-     */
-    val width: Int = metrics.widthPixels
-
-    /**
-     * 展示高度，不包含虚拟键与状态栏，单位px
-     */
-    val height: Int = height() - status
-
-    /**
-     * 展示高度，包含状态栏，不包含虚拟键，单位px
-     */
-    fun height(): Int = metrics.heightPixels
-
 }
