750486b94d9b3ff093e85649fccfc5400042cd9f
lijiawei11 <lijiawei11@baidu.com>
2022-12-08 11:18:38 +0800

    update pdfjs module klint code.

--- KotlinLibraryX/app-pdfjs/src/main/java/com/lee/pdfjs/MainActivity.kt
+++ KotlinLibraryX/app-pdfjs/src/main/java/com/lee/pdfjs/MainActivity.kt
@@ -3,21 +3,21 @@ package com.lee.pdfjs
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.webkit.JavascriptInterface
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.lee.library.widget.AppWebView
 import java.io.File
 
+@SuppressLint("SetJavaScriptEnabled")
 class MainActivity : AppCompatActivity() {
 
     private val web by lazy { findViewById<AppWebView>(R.id.web) }
     private val progress by lazy { findViewById<ProgressBar>(R.id.progress) }
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,24 +25,29 @@ class MainActivity : AppCompatActivity() {
         web.settings.run {
             javaScriptEnabled = true
             allowFileAccess = true
-            allowFileAccessFromFileURLs = true
-            allowUniversalAccessFromFileURLs = true
-
-            setSupportZoom(true)
             builtInZoomControls = true
             useWideViewPort = true
+            allowFileAccessFromFileURLs = true
+            allowUniversalAccessFromFileURLs = true
+            setSupportZoom(true)
         }
         web.addJavascriptInterface(this, "control")
 
-        //"file:///android_asset/viewer.html?${Uri.fromFile(File(""))}"
-//        web.loadUrl("file:///android_asset/viewer.html?" + "file:///android_asset/book.pdf")
-        val file = File(filesDir.absolutePath + File.separator + "book.pdf")
+        loadAssets()
+    }
+
+    private fun loadFile(path: String = filesDir.absolutePath + File.separator + "book.pdf") {
+        val file = File(path)
         val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             FileProvider.getUriForFile(this, "$packageName.fileprovider", file)
         } else {
             Uri.fromFile(file)
         }
         web.loadUrl("file:///android_asset/viewer.html?$uri")
+    }
+
+    private fun loadAssets() {
+        web.loadUrl("file:///android_asset/viewer.html?" + "file:///android_asset/book.pdf")
     }
 
     @JavascriptInterface
