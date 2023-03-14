949e954a2d1c8e4aecc2001569f12b6902c472e7
lijiawei11 <lijiawei11@baidu.com>
2023-03-14 10:46:35 +0800

    update BaseViewHolder.java to BaseViewHolder.kt

--- KotlinLibraryX/library/src/main/java/com/lee/library/adapter/base/BaseViewHolder.kt
+++ KotlinLibraryX/library/src/main/java/com/lee/library/adapter/base/BaseViewHolder.kt
@@ -1,13 +1,11 @@
-package com.lee.library.adapter.base;
+package com.lee.library.adapter.base
 
-import android.content.Context;
-import android.util.SparseArray;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context
+import android.util.SparseArray
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * 封装的RecyclerViewHolder
@@ -15,45 +13,14 @@ import androidx.recyclerview.widget.RecyclerView;
  * @author jv.lee
  * @date 2019/3/29
  */
-public class BaseViewHolder extends RecyclerView.ViewHolder {
+open class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val mViews = SparseArray<View>()
 
     /**
-     * 所有的控件的集合
-     */
-    private final SparseArray<View> mViews;
-    /**
-     * 当前View对象
-     */
-    private final View mConvertView;
-
-    public BaseViewHolder(@NonNull View itemView) {
-        super(itemView);
-        mConvertView = itemView;
-        mViews = new SparseArray<>();
-    }
-
-    /**
-     * 创建RViewHolder对象
-     * 创建RViewHolder对象
-     *
-     * @param context  上下文对象
-     * @param parent   父容器
-     * @param layoutId 布局id
-     * @return 返回ViewHolder
-     */
-    public static BaseViewHolder createViewHolder(Context context, ViewGroup parent, int layoutId) {
-        View itemView = LayoutInflater.from(context).inflate(layoutId, parent, false);
-        return new BaseViewHolder(itemView);
-    }
-
-    /**
-     * 获取当前View
-     *
      * @return 返回当前itemView
      */
-    public View getConvertView() {
-        return mConvertView;
-    }
+    fun getConvertView() = itemView
 
     /**
      * 通过R.id.xxx获取的某个控件
@@ -62,14 +29,21 @@ public class BaseViewHolder extends RecyclerView.ViewHolder {
      * @param <T>    业务类型
      * @return 返回通过资源id获取的控件
      */
-    @SuppressWarnings("unchecked")
-    public <T extends View> T getView(int viewId) {
-        View view = mViews.get(viewId);
+    @Suppress("UNCHECKED_CAST")
+    fun <T : View> getView(viewId: Int): T {
+        var view = mViews[viewId]
         if (view == null) {
-            view = mConvertView.findViewById(viewId);
-            mViews.put(viewId, view);
+            view = itemView.findViewById(viewId)
+            mViews.put(viewId, view)
         }
-        return (T) view;
+        return view as T
+    }
+
+    companion object {
+        fun createViewHolder(context: Context, parent: ViewGroup, layoutId: Int): BaseViewHolder {
+            val itemView = LayoutInflater.from(context).inflate(layoutId, parent, false)
+            return BaseViewHolder(itemView)
+        }
     }
 
 }
