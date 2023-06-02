48bf5bc84f6f0306a2f3afa71c0267053c9b770f
lijiawei11 <lijiawei11@baidu.com>
2023-06-02 10:30:56 +0800

    update WindowViewHelper.kt description code.

--- KotlinLibraryX/app-ui/src/main/java/com/lee/ui/utils/WindowViewHelper.kt
+++ KotlinLibraryX/app-ui/src/main/java/com/lee/ui/utils/WindowViewHelper.kt
@@ -8,6 +8,7 @@ import android.view.WindowManager
 
 /**
  * 窗口自定义view 帮助类
+ * 可以在Activity中窗口顶部通过 WindowManager 绘制view到屏幕上
  * @author jv.lee
  * @date 2023/6/1
  */
@@ -21,6 +22,10 @@ class WindowViewHelper private constructor(
     private var contentView: View? = null
     private var windowManager: WindowManager? = null
 
+    /**
+     * 通过WindowManager 显示自定义view
+     * @param contentView 需要显示的view
+     */
     fun show(contentView: View) {
         hide()
         this.windowManager =
@@ -35,6 +40,9 @@ class WindowViewHelper private constructor(
         }
     }
 
+    /**
+     * 通过WindowManager 隐藏自定义view
+     */
     fun hide() {
         windowManager?.run {
             contentView?.let { view ->
@@ -45,6 +53,11 @@ class WindowViewHelper private constructor(
         contentView = null
     }
 
+    /**
+     * 通过WindowManager 显示自定义view并延时自动隐藏
+     * @param contentView 需要显示的view
+     * @param time 延时时间
+     */
     fun showDelayHide(contentView: View, time: Long = 3000) {
         show(contentView)
         contentView.postDelayed({ hide() }, time)
@@ -70,31 +83,56 @@ class WindowViewHelper private constructor(
         private var x = 0
         private var y = 0
 
+        /**
+         * 设置WindowManager.LayoutParams的宽度
+         * @param width 宽度
+         */
         fun setWidth(width: Int): Builder {
             this.width = width
             return this
         }
 
+        /**
+         * 设置WindowManager.LayoutParams的高度
+         * @param height 高度
+         */
         fun setHeight(height: Int): Builder {
             this.height = height
             return this
         }
 
+        /**
+         * 设置WindowManager.LayoutParams的gravity位置
+         * Gravity.CENTER: 居中
+         * Gravity.START or Gravity.TOP: 左上角开始布局，该模式设置x，y有效
+         * @param gravity 位置
+         */
         fun setGravity(gravity: Int): Builder {
             this.gravity = gravity
             return this
         }
 
+        /**
+         * 设置WindowManager.LayoutParams的x坐标
+         * @param x x坐标
+         */
         fun setX(x: Int): Builder {
             this.x = x
             return this
         }
 
+        /**
+         * 设置WindowManager.LayoutParams的y坐标
+         * @param y y坐标
+         */
         fun setY(y: Int): Builder {
             this.y = y
             return this
         }
 
+        /**
+         * 构建WindowViewHelper实例
+         */
         fun build(): WindowViewHelper {
             return WindowViewHelper(width, height, gravity, x, y)
         }
