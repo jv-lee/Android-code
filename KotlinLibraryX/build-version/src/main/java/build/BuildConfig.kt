2334636bf0973b6fdda6e42fdfbfb18068f0a7c5
lijw17 <lijw17@xiaopeng.com>
2024-11-11 10:16:23 +0800

    update version.

--- KotlinLibraryX/build-version/src/main/java/build/BuildConfig.kt
+++ KotlinLibraryX/build-version/src/main/java/build/BuildConfig.kt
@@ -8,9 +8,9 @@ package build
 object BuildConfig {
     const val APPLICATION_ID = "com.core.lee"
 
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 35
     const val MIN_SDK = 21
-    const val TARGET_SDK = 34
+    const val TARGET_SDK = 35
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
 
